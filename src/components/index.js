c1e687d358b73e84b38a599235ab203866eea5b8
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-06-25 04:55:18 +0300

    start the initial setup and create files and folders sturcture

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import SubHeading from "./SubHeading/SubHeading";
+import Newsletter from "./Footer/news-letter/Newsletter";
+import FooterOverlay from "./Footer/footer-overlay/FooterOverlay";
+import MenuItem from "./menuitem/MenuItem";
+import Navbar from "./navbar/Navbar";
+
+export { SubHeading, Newsletter, FooterOverlay, MenuItem, Navbar };
---
+++ src/container/index.js
@@ -0,0 +1,21 @@
+import AboutUs from "./about-us/AboutUs";
+import Chef from "./chef/Chef";
+import FindUs from "./findus/FindUs";
+import Footer from "./footer/Footer";
+import Gallery from "./gallery/Gallery";
+import Header from "./header/Header";
+import Intro from "./intro/Intro";
+import Laurels from "./laurels/Laurels";
+import SpecialMenu from "./menu/SpecialMenu";
+
+export {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+};
